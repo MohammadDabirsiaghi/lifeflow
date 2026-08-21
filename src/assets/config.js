4c255010438f3764f98bebb5b2b00c481fa7031c
dabirsiaghim <dabirsiaghim@gmail.com>
2026-08-21 14:17:59 +0330

    s

--- src/assets/config.js
+++ src/assets/config.js
@@ -2,7 +2,7 @@
 
 window.APP_CONFIG = Object.freeze({
     API_URL:
-        "https://script.google.com/macros/s/AKfycbxalSIb2GOOuO1zDZlf4T0oAoMZGxw382Y3gRLTAaasAVuARZPypb3Je4nz-Ma4ZyQmdg/exec"
+        "https://script.google.com/macros/s/AKfycbzpBrO3hS8oe68GtwgQqe1bo4qQHoOmw3uHJyYCDejYS3C7lp9pfN7Xu4P3qazc9bhNFw/exec"
 });
 
 // https://script.google.com/macros/s/AKfycbykl_pfchi6YsUe_5ybA7jEhc9AW633Zds_BJj-DxNSihZ3o5a7mT3TNbaSZHT2c6XAIw/exec
